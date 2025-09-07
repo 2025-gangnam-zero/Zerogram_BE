5c7bd8aa8550d6c70c38dde68afddf65281311b5
narahub123 <parknara0213@gmail.com>
2025-09-07 14:46:32 +0900

    Feat: ecosystem.config.js 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,6 @@ module.exports = {
       script: "dist/server.js",
       instances: "max",
       exec_mode: "cluster",
-      // 서버에 있는 .env를 선로딩 (레포에 비밀 넣지 않기)
-      node_args: "-r dotenv/config",
       env: {
         NODE_ENV: "prod",
         PORT: "4000",
