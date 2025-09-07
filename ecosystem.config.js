ba120d1934f2f5e46fe284da8209256771ceec2f
narahub123 <parknara0213@gmail.com>
2025-09-07 14:15:10 +0900

    Feat: deploy.yml 수정 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,17 @@ module.exports = {
     {
       name: "zerogram",
       script: "dist/server.js",
-      instances: 1,
-      exec_mode: "fork",
+      instances: "max",
+      exec_mode: "cluster",
+      // 서버에 있는 .env를 선로딩 (레포에 비밀 넣지 않기)
+      node_args: "-r dotenv/config",
       env: {
         NODE_ENV: "prod",
         PORT: "4000",
+        DOTENV_CONFIG_PATH: "/var/www/zerogram/shared/.env",
       },
+      listen_timeout: 10000,
+      kill_timeout: 5000,
     },
   ],
 };
