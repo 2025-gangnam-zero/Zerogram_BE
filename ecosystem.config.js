586210407d40abd5823e843a42a6846b7a2a3484
narahub123 <parknara0213@gmail.com>
2025-09-24 19:06:52 +0900

    ecosystem.config.js 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
         NODE_ENV: "prod",
         PORT: "4000",
         DOTENV_CONFIG_PATH: "/var/www/zerogram/shared/.env",
+        AWS_REGION: "ap-northeast-2",
       },
       listen_timeout: 10000,
       kill_timeout: 5000,
